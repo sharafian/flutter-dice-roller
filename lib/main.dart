2ab8550970acf7d1ed15dec6952f322ebf943998
sharafian <ben@sharafian.me>
2019-10-16 18:22:46 -0700

    fix: keep bloc state in stateful component

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import './roller_page.dart';
 import './rollers_provider.dart';
+import './rollers_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(RollerApp());
 
-class MyApp extends StatelessWidget {
+class RollerAppState extends State<RollerApp> {
+  RollersBloc rollersBloc;
+
+  @override
+  void initState() {
+    rollersBloc = RollersBloc();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return RollersProvider(
+      rollersBloc: rollersBloc,
       child: MaterialApp(
         title: 'Dice Roller',
         theme: ThemeData(
@@ -17,4 +27,9 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+class RollerApp extends StatefulWidget {
+  @override
+  RollerAppState createState() => RollerAppState();
 }
